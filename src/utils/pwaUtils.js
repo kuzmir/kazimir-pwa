7354f3ed049718ba877c27ce094feab8f3235bda
vergilius <michal.pawlinski@arteverest.pl>
2019-06-15 09:44:00 +0200

    Cleanup deps

--- src/utils/pwaUtils.js
+++ src/utils/pwaUtils.js
@@ -1,16 +1,16 @@
 export const initServiceWorker = () => {
   if (navigator.serviceWorker) {
     const options = {
-      scope: '/'
+      scope: '/',
     };
 
     navigator.serviceWorker
       .register('/serviceWorker.js', options)
       .then(registration => {
-        console.info('Service worker regitstration success', registration)
+        console.info('Service worker regitstration success', registration); // eslint-disable-line no-console
       })
       .catch(error => {
-        console.error('Service worker regitstration error', error)
+        console.error('Service worker regitstration error', error);
       });
   }
 };
