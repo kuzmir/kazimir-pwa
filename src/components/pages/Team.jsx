33d66106c0f99e0a099be812cf665b51a88bd20a
patrycjaradaczynska <patrycja.radaczynska@brainly.com>
2019-10-14 22:43:39 +0200

    Update team list

--- src/components/pages/Team.jsx
+++ src/components/pages/Team.jsx
@@ -11,10 +11,56 @@ type TeamPropsType = {
 
 const Team = ({translate}: TeamPropsType) => {
   const [modalVisible, showModal] = useState(false);
+  const [itemShown, setActiveItem] = useState(null);
 
-  const openModal = () => {
+  const openModal = id => {
     modalVisible ? showModal(false) : showModal(true);
+    itemShown ? setActiveItem(null) : setActiveItem(id);
+    console.log('open modal', id, itemShown);
   };
+
+  const array = [
+    {
+      thumb: 'prada_thumb.jpg',
+      large: 'prada_large.jpg',
+    },
+    {
+      thumb: 'emaj_thumb.jpg',
+      large: 'emaj_large.jpg',
+    },
+    {
+      thumb: 'krzysiek_thumb.jpg',
+      large: 'krzysiek_large.jpg',
+    },
+    {
+      thumb: 'jan_thumb.jpg',
+      large: 'jan_large.jpg',
+    },
+    {
+      thumb: 'marta_thumb.jpg',
+      large: 'marta_large.jpg',
+    },
+    {
+      thumb: 'piotr_thumb.jpg',
+      large: 'piotr_large.jpg',
+    },
+    {
+      thumb: 'kasia_thumb.jpg',
+      large: 'kasia_large.jpg',
+    },
+    {
+      thumb: 'wika_thumb.jpg',
+      large: 'wika_large.jpg',
+    },
+    {
+      thumb: 'ania_thumb.jpg',
+      large: 'ania_large.jpg',
+    },
+    {
+      thumb: 'bartek_thumb.jpg',
+      large: 'bartek_large.jpg',
+    },
+  ];
 
   return (
     <>
@@ -23,96 +69,22 @@ const Team = ({translate}: TeamPropsType) => {
         <h1 className={style.headline}>Team</h1>
 
         <div className={style.teamFlexContainer}>
-          <div className={style.teamItem} onClick={openModal}>
-            <img src="../../images/team/prada_thumb.jpg" />
-            <Modal
-              modalVisible={modalVisible}
-              name={translate('TEAM_1_NAME')}
-              info={translate('TEAM_1_CONTENT')}
-              imagePath="../../images/team/prada_large.jpg"
-            />
-          </div>
-          <div className={style.teamItem} onClick={openModal}>
-            <img src="../../images/team/emaj_thumb.jpg" />
-            <Modal
-              modalVisible={modalVisible}
-              name={translate('TEAM_2_NAME')}
-              info={translate('TEAM_2_CONTENT')}
-              imagePath="../../images/team/emaj_large.jpg"
-            />
-          </div>
-          <div className={style.teamItem} onClick={openModal}>
-            <img src="../../images/team/krzysiek_thumb.jpg" />
-            <Modal
-              modalVisible={modalVisible}
-              name={translate('TEAM_3_NAME')}
-              info={translate('TEAM_3_CONTENT')}
-              imagePath="../../images/team/krzysiek_large.jpg"
-            />
-          </div>
-          <div className={style.teamItem} onClick={openModal}>
-            <img src="../../images/team/jan_thumb.jpg" />
-            <Modal
-              modalVisible={modalVisible}
-              name={translate('TEAM_4_NAME')}
-              info={translate('TEAM_4_CONTENT')}
-              imagePath="../../images/team/jan_large.jpg"
-            />
-          </div>
-          <div className={style.teamItem} onClick={openModal}>
-            <img src="../../images/team/marta_thumb.jpg" />
-            <Modal
-              modalVisible={modalVisible}
-              name={translate('TEAM_5_NAME')}
-              info={translate('TEAM_5_CONTENT')}
-              imagePath="../../images/team/marta_large.jpg"
-            />
-          </div>
-          <div className={style.teamItem} onClick={openModal}>
-            <img src="../../images/team/piotr_thumb.jpg" />
-            <Modal
-              modalVisible={modalVisible}
-              name={translate('TEAM_6_NAME')}
-              info={translate('TEAM_6_CONTENT')}
-              imagePath="../../images/team/piotr_large.jpg"
-            />
-          </div>
-          <div className={style.teamItem} onClick={openModal}>
-            <img src="../../images/team/kasia_thumb.jpg" />
-            <Modal
-              modalVisible={modalVisible}
-              name={translate('TEAM_7_NAME')}
-              info={translate('TEAM_7_CONTENT')}
-              imagePath="../../images/team/kasia_large.jpg"
-            />
-          </div>
-          <div className={style.teamItem} onClick={openModal}>
-            <img src="../../images/team/wika_thumb.jpg" />
-            <Modal
-              modalVisible={modalVisible}
-              name={translate('TEAM_8_NAME')}
-              info={translate('TEAM_8_CONTENT')}
-              imagePath="../../images/team/wika_large.jpg"
-            />
-          </div>
-          <div className={style.teamItem} onClick={openModal}>
-            <img src="../../images/team/ania_thumb.jpg" />
-            <Modal
-              modalVisible={modalVisible}
-              name={translate('TEAM_9_NAME')}
-              info={translate('TEAM_9_CONTENT')}
-              imagePath="../../images/team/ania_large.jpg"
-            />
-          </div>
-          <div className={style.teamItem} onClick={openModal}>
-            <img src="../../images/team/bartek_thumb.jpg" />
-            <Modal
-              modalVisible={modalVisible}
-              name={translate('TEAM_10_NAME')}
-              info={translate('TEAM_10_CONTENT')}
-              imagePath="../../images/team/bartek_large.jpg"
-            />
-          </div>
+          {array.map((item, index) => (
+            <div
+              className={style.teamItem}
+              onClick={() => openModal(index)}
+              key={index}
+            >
+              <img src={`../../images/team/${item.thumb}`} />
+              <Modal
+                id={itemShown}
+                modalVisible={modalVisible}
+                name={translate(`TEAM_${index}_NAME`)}
+                info={translate(`TEAM_${index}_CONTENT`)}
+                imagePath={`../../images/team/${item.large}`}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </>
