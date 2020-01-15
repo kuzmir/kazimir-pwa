6e4e9166e75c99792958081d6f04cf9415764c82
patrycjaradaczynska <patrycja.radaczynska@brainly.com>
2020-01-15 18:29:30 +0100

    Remove comments

--- src/components/navigationIcons/ListIcon.jsx
+++ src/components/navigationIcons/ListIcon.jsx
@@ -7,19 +7,6 @@ type IconPropsType = {
 };
 
 const ListIcon = ({color}: IconPropsType) => (
-  // <svg
-  //   xmlns="http://www.w3.org/2000/svg"
-  //   width="24"
-  //   height="24"
-  //   viewBox="0 0 24 24"
-  //   fill={color}
-  // >
-  //   <g fill="none">
-  //     <path d="M0 0h24v24H0V0z" />
-  //     <path opacity=".87" d="M0 0h24v24H0V0z" />
-  //   </g>
-  //   <path d="M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7zm-4 6h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7z" />
-  // </svg>
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -150 600 900" fill={color}>
     <path d="M540,70H60c-5.523,0-10-4.477-10-10s4.477-10,10-10h480c5.522,0,10,4.477,10,10S545.522,70,540,70z" />
     <path d="M480,230H60c-5.523,0-10-4.477-10-10s4.477-10,10-10h420c5.522,0,10,4.477,10,10S485.522,230,480,230z" />
