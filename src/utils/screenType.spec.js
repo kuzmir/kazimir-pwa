92bf1f111f5232ee85b8145c6737bfab391cf266
vergilius <michal.pawlinski@arteverest.pl>
2020-01-16 21:30:56 +0100

    Add unit

---
+++ src/utils/screenType.spec.js
@@ -0,0 +1,17 @@
+// @flow
+
+import {getScreenType, SCREEN_MOBILE, SCREEN_DESKTOP} from './screenType';
+
+describe('getScreenType()', () => {
+  it ('returns "SCREEN_MOBILE" when viewport is smaller than breakpoint', () => {
+    global.window.innerWidth = 480;
+
+    expect(getScreenType()).toEqual(SCREEN_MOBILE);
+  });
+
+  it ('returns "SCREEN_DESKTOP" when viewport is greater than breakpoint', () => {
+    global.window.innerWidth = 1280;
+
+    expect(getScreenType()).toEqual(SCREEN_DESKTOP);
+  });
+});
